bd189e42330483b5e500b95df985e3f7a42f98b9
Drishya Dobriyal <dobriyaldrishya@gmail.com>
2020-05-02 12:02:16 +0530

    refactored code

--- numbers.c
+++ numbers.c
@@ -38,79 +38,67 @@ int main(void) {
       printf("Enter number:\n");
       scanf("%d", &value);
       print_status(add_to_end(list, value));
-      display_menu();
       break;
     
     case 'b' : 
       printf("Enter number:\n");
       scanf("%d", &value);
       print_status(add_to_start(list, value));
-      display_menu();
       break;
 
     case 'c' : 
       printf("Enter number and position:\n");
       scanf("%d %d", &value, &position);
       print_status(insert_at(list, value, position - 1));
-      display_menu();
       break;
 
     case 'd' :
       printf("Enter number:\n");
       scanf("%d", &value);
       print_status(add_unique(list, value));
-      display_menu();
       break;
     
     case 'e' :
       print_status(remove_from_start(list));
-      display_menu();
       break;
     
     case 'f' :
       print_status(remove_from_end(list));
-      display_menu();
       break;
 
     case 'g' :
       printf("Enter position:\n");
       scanf("%d",&position);
       print_status(remove_at(list, position));
-      display_menu();
       break;
 
     case 'h' :
       printf("Enter number:\n");
       scanf("%d", &value);
       print_status(remove_first_occurrence(list, value));
-      display_menu();
       break;
 
     case 'i' :
       printf("Enter number:\n");
       scanf("%d", &value);
       print_status(remove_all_occurrences(list, value));
-      display_menu();
       break;
 
     case 'j' :
       print_status(clear_list(list));
-      display_menu();
       break;
 
     case 'k' :
       printf("Enter number:\n");
       scanf("%d", &value);
       printf("%s\n",is_value_present(list, value) == Success ? "Present" : "Not Present");
-      display_menu();
       break;
 
     case 'l' : 
       display(list);
-      // display_menu();
       break;
     }  
-
+    display_menu();
     while(getchar() != '\n');
     ch = getchar();
   }
